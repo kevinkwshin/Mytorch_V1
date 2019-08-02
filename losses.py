78abf72e4777f8b31995166c3eec411dbec94c0d
Keewon Shin <kevinkwshin@gmail.com>
2019-08-02 19:06:37 +0900

    Update losses.py

--- losses.py
+++ losses.py
@@ -2,56 +2,56 @@ import torch
 import torch.nn as nn
 from torch.nn import Module
 
-# def dice_loss(input, target):
-#     """Dice loss.
-#     :param input: The input (predicted)
-#     :param target:  The target (ground truth)
-#     :returns: the Dice score between 0 and 1.
-#     """
-#     eps = 0.0001
-
-#     iflat = input.view(-1)
-#     tflat = target.view(-1)
-
-#     intersection = (iflat * tflat).sum()
-#     union = iflat.sum() + tflat.sum()
-
-#     dice = (2.0 * intersection + eps) / (union + eps)
-#     return - dice
-
-def dice_loss(true, logits, eps=1e-7):
-    """Computes the Sørensen–Dice loss.
-    Note that PyTorch optimizers minimize a loss. In this
-    case, we would like to maximize the dice loss so we
-    return the negated dice loss.
-    Args:
-        true: a tensor of shape [B, 1, H, W].
-        logits: a tensor of shape [B, C, H, W]. Corresponds to
-            the raw output or logits of the model.
-        eps: added to the denominator for numerical stability.
-    Returns:
-        dice_loss: the Sørensen–Dice loss.
+def dice_loss(input, target):
+    """Dice loss.
+    :param input: The input (predicted)
+    :param target:  The target (ground truth)
+    :returns: the Dice score between 0 and 1.
     """
-    num_classes = logits.shape[1]
-    if num_classes == 1:
-        true_1_hot = torch.eye(num_classes + 1)[true.squeeze(1)]
-        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
-        true_1_hot_f = true_1_hot[:, 0:1, :, :]
-        true_1_hot_s = true_1_hot[:, 1:2, :, :]
-        true_1_hot = torch.cat([true_1_hot_s, true_1_hot_f], dim=1)
-        pos_prob = torch.sigmoid(logits)
-        neg_prob = 1 - pos_prob
-        probas = torch.cat([pos_prob, neg_prob], dim=1)
-    else:
-        true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
-        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
-        probas = F.softmax(logits, dim=1)
-    true_1_hot = true_1_hot.type(logits.type())
-    dims = (0,) + tuple(range(2, true.ndimension()))
-    intersection = torch.sum(probas * true_1_hot, dims)
-    cardinality = torch.sum(probas + true_1_hot, dims)
-    dice_loss = (2. * intersection / (cardinality + eps)).mean()
-    return (1 - dice_loss)
+    eps = 0.0001
+
+    iflat = input.view(-1)
+    tflat = target.view(-1)
+
+    intersection = (iflat * tflat).sum()
+    union = iflat.sum() + tflat.sum()
+
+    dice = (2.0 * intersection + eps) / (union + eps)
+    return - dice
+
+# def dice_loss(true, logits, eps=1e-7):
+#     """Computes the Sørensen–Dice loss.
+#     Note that PyTorch optimizers minimize a loss. In this
+#     case, we would like to maximize the dice loss so we
+#     return the negated dice loss.
+#     Args:
+#         true: a tensor of shape [B, 1, H, W].
+#         logits: a tensor of shape [B, C, H, W]. Corresponds to
+#             the raw output or logits of the model.
+#         eps: added to the denominator for numerical stability.
+#     Returns:
+#         dice_loss: the Sørensen–Dice loss.
+#     """
+#     num_classes = logits.shape[1]
+#     if num_classes == 1:
+#         true_1_hot = torch.eye(num_classes + 1)[true.squeeze(1)]
+#         true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+#         true_1_hot_f = true_1_hot[:, 0:1, :, :]
+#         true_1_hot_s = true_1_hot[:, 1:2, :, :]
+#         true_1_hot = torch.cat([true_1_hot_s, true_1_hot_f], dim=1)
+#         pos_prob = torch.sigmoid(logits)
+#         neg_prob = 1 - pos_prob
+#         probas = torch.cat([pos_prob, neg_prob], dim=1)
+#     else:
+#         true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
+#         true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+#         probas = F.softmax(logits, dim=1)
+#     true_1_hot = true_1_hot.type(logits.type())
+#     dims = (0,) + tuple(range(2, true.ndimension()))
+#     intersection = torch.sum(probas * true_1_hot, dims)
+#     cardinality = torch.sum(probas + true_1_hot, dims)
+#     dice_loss = (2. * intersection / (cardinality + eps)).mean()
+#     return (1 - dice_loss)
 
 class MaskedDiceLoss(Module):
     """A masked version of the Dice loss.
