1e50003ec51b6a7a7430a030f333a5065491b4ba
Keewon Shin <kevinkwshin@gmail.com>
2019-08-02 19:57:23 +0900

    Update losses.py

--- losses.py
+++ losses.py
@@ -18,7 +18,31 @@ def dice_loss(input, target):
 
     dice = (2.0 * intersection + eps) / (union + eps)
     return - dice
-
+class MulticlassDiceLoss(nn.Module):
+	"""
+	requires one hot encoded target. Applies DiceLoss on each class iteratively.
+	requires input.shape[0:1] and target.shape[0:1] to be (N, C) where N is
+	  batch size and C is number of classes
+	"""
+	def __init__(self):
+		super(MulticlassDiceLoss, self).__init__()
+ 
+	def forward(self, input, target, weights=None):
+ 
+		C = target.shape[1]
+ 
+		# if weights is None:
+		# 	weights = torch.ones(C) #uniform weights for all classes
+ 
+		dice = DiceLoss()
+		totalLoss = 0
+ 
+		for i in range(C):
+			diceLoss = dice(input[:,i], target[:,i])
+			if weights is not None:
+				diceLoss *= weights[i]
+			totalLoss += diceLoss
+ 
 # def dice_loss(true, logits, eps=1e-7):
 #     """Computes the Sørensen–Dice loss.
 #     Note that PyTorch optimizers minimize a loss. In this
