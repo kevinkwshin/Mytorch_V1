8943b747f6f282c10883f9e73dc4070d55eed7d6
Keewon Shin <kevinkwshin@gmail.com>
2019-08-02 17:07:22 +0900

    Update scores.py

--- scores.py
+++ scores.py
@@ -75,3 +75,37 @@ def accuracy_score(prediction, groundtruth):
     N = FP + FN + TP + TN
     accuracy = np.divide(TP + TN, N)
     return accuracy * 100.0
+
+def threshold_predictions(predictions, thr=0.5):
+    thresholded_preds = predictions[:]
+    low_values_indices = thresholded_preds < thr
+    thresholded_preds[low_values_indices] = 0
+    low_values_indices = thresholded_preds >= thr
+    thresholded_preds[low_values_indices] = 1
+    return thresholded_preds
+
+def numeric_score(prediction, groundtruth):
+    FP = np.float(np.sum((prediction == 1) & (groundtruth == 0)))
+    FN = np.float(np.sum((prediction == 0) & (groundtruth == 1)))
+    TP = np.float(np.sum((prediction == 1) & (groundtruth == 1)))
+    TN = np.float(np.sum((prediction == 0) & (groundtruth == 0)))
+    return FP, FN, TP, TN 
+  
+def accuracy(prediction, groundtruth):
+    FP, FN, TP, TN = numeric_score(prediction, groundtruth)
+    N = FP + FN + TP + TN
+    accuracy = np.divide(TP + TN, N)
+    return accuracy * 100.0
+
+def metric_scores_summary(prediction, groundtruth,threshold=0.5):
+    prediction = threshold_predictions(prediction,threshold)
+    
+    dice = dice_score(prediction, groundtruth)
+    precision = precision_score(prediction, groundtruth)
+    recall = recall_score(prediction, groundtruth)
+    specificity = specificity_score(prediction, groundtruth)
+    iou = intersection_over_union(prediction, groundtruth)
+    accuracy = accuracy_score(prediction, groundtruth)
+#     hausdorff = hausdorff_score(prediction, groundtruth)
+    print("DSC {:.2f} PRECISION {:.2f} RECALL {:.2f} SPECIFICITY {:.2f} IOU {:.2f} ACCURACY {:.2f}".format(dice,precision,recall,specificity,iou,accuracy))
+    return dice,precision,recall,specificity,iou,accuracy
