78101281115018980e4cbc613be7e8edaecdfc53
Keewon Shin <kevinkwshin@gmail.com>
2019-09-02 11:45:12 +0900

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,28 +1,36 @@
-
 def model_eval():
-  for idx, batch in tqdm_notebook(enumerate(valid_loader)):
-      image = batch['inputs'].to(device=device, dtype=torch.float)
-      mask  = batch['labels'].to(device=device, dtype=torch.float)
+    for idx, batch in enumerate(valid_loader):
+        image = batch['image'].to(device=device, dtype=torch.float)
+        mask  = batch['mask'].to(device=device, dtype=torch.float)
 
-      with torch.no_grad():
-          preds = model(image)
+        with torch.no_grad():
+            preds = model(image)
 
-      # resize
-      image_depth = batch['original_shape'][0].item()
-      image_height= batch['original_shape'][1].item()
-      image_width = batch['original_shape'][2].item()
+        # post processing VOXEL REMOVE?
 
-      image_=image_resize_3D(image.squeeze().cpu().detach().numpy(),image_depth,image_height,image_width)
-      preds_=image_resize_3D(preds.squeeze().cpu().detach().numpy(),image_depth,image_height,image_width)
-      mask_ =image_resize_3D(mask.squeeze().cpu().detach().numpy(),image_depth,image_height,image_width)
+        # resize
+        image_depth = batch['original_shape'][2].item()
+        image_height= batch['original_shape'][0].item()
+        image_width = batch['original_shape'][1].item()    
 
-      # post processing VOXEL REMOVE?
+        image_=image_resize_3D(image.squeeze().cpu().detach().numpy(),image_depth,image_height,image_width)
 
-      metric_scores_summary(preds_.squeeze(),mask_.squeeze(),threshold=0.3,print_score=True)
+        preds[preds<=0.5] = 0
+        preds[preds>0.5] = 1
 
-      print(idx,batch['filename'],'mask',np.unique(mask_,return_counts=True))
-      print(idx,batch['filename'],'pred',np.unique(label_threshold(preds_,0.3),return_counts=True))
+        if preds.shape[1] > 1:
+            print('multi?')
+            preds_=image_resize_3D(label_onehot_decode(preds.squeeze().cpu().detach().numpy()),image_depth,image_height,image_width)
+            mask_ =image_resize_3D(label_onehot_decode(mask.squeeze().cpu().detach().numpy()),image_depth,image_height,image_width)
+        else:
+            print('single?')
+            preds_=image_resize_3D(preds.squeeze().cpu().detach().numpy(),image_depth,image_height,image_width)
+            mask_ =image_resize_3D(mask.squeeze().cpu().detach().numpy(),image_depth,image_height,image_width)
 
-  #     image_save_nii(label_threshold(preds_,0.44),'Output/'+str(batch['filename'][0])+'_preds.nii.gz')
-  #     image_save_nii(mask_,'Output/'+str(batch['filename'][0])+'_mask.nii.gz')
-      torch.cuda.empty_cache()
+    #     metric_scores_summary(preds.squeeze(),mask.squeeze(),0.3)
+    #     metric_scores_summary(preds,mask,0.5)
+
+        image_save_nii(image_,'Output/'+str(batch['filename'][0])+'_image.nii.gz')
+        image_save_nii(preds_,'Output/'+str(batch['filename'][0])+'_preds.nii.gz')
+        image_save_nii(mask_,'Output/'+str(batch['filename'][0])+'_mask.nii.gz')
+        torch.cuda.empty_cache()
