084e27bad9508811e8904f7d04da8d1bdfac19c4
Keewon Shin <kevinkwshin@gmail.com>
2019-10-08 11:07:50 +0900

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -292,14 +292,15 @@ class MRI2DSegmentationDataset(Dataset):
         # Using the "Float mode" of PIL, the only mode
         # supporting unbounded float32 values
 #         input_img = Image.fromarray(pair_slice["input"], mode='F')
-        print(type(pair_slice))
-        input_img = pair_slice["input"]
+#         print(type(pair_slice))
+        input_img = np.array(pair_slice["input"])
 
         # Handle unlabeled data
         if pair_slice["gt"] is None:
             gt_img = None
         else:
-            gt_img = Image.fromarray(pair_slice["gt"], mode='F')
+#             gt_img = Image.fromarray(pair_slice["gt"], mode='F')
+            gt_img = np.array(pair_slice["gt"])
 
         data_dict = {
             'input': input_img,
