19099c333e437bb5b20a4f64994b10372442197b
Keewon Shin <kevinkwshin@gmail.com>
2019-10-08 12:42:56 +0900

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -75,15 +75,15 @@ class SegmentationPair2D(object):
         self.canonical = canonical
         self.cache = cache
 
-        self.input_handle = nib.load(self.input_filename)
-#         self.input_handle = sitk.GetArrayFromImage(sitk.ReadImage(self.input_filename)).astype('float32')
+#         self.input_handle = nib.load(self.input_filename)
+        self.input_handle = sitk.GetArrayFromImage(sitk.ReadImage(self.input_filename)).astype('float32')
 
         # Unlabeled data (inference time)
         if self.gt_filename is None:
             self.gt_handle = None
         else:
-            self.gt_handle = nib.load(self.gt_filename)     
-#             self.gt_handle  = sitk.GetArrayFromImage(sitk.ReadImage(self.gt_filename)).astype('float32')
+#             self.gt_handle = nib.load(self.gt_filename)     
+            self.gt_handle  = sitk.GetArrayFromImage(sitk.ReadImage(self.gt_filename)).astype('float32')
 
         if len(self.input_handle.shape) > 3:
             raise RuntimeError("4-dimensional volumes not supported.")
