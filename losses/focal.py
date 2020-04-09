f54330f90e87302dfb563eac115c3c8dae1ce96f
Keewon Shin <kevinkwshin@gmail.com>
2020-04-10 07:53:25 +0900

    Update focal.py

--- losses/focal.py
+++ losses/focal.py
@@ -3,7 +3,6 @@ from torch import nn
 
 EPSILON = 1e-6
 
-
 class FocalLoss2d(nn.Module):
     def __init__(self, gamma=2, ignore_index=255):
         super().__init__()
@@ -13,6 +12,8 @@ class FocalLoss2d(nn.Module):
     def forward(self, outputs: torch.Tensor, targets: torch.Tensor):
         outputs = outputs.contiguous()
         targets = targets.contiguous()
+        print(outputs.shape,targets.shape)
+        
         eps = 1e-8
         non_ignored = targets.view(-1) != self.ignore_index
         targets = targets.view(-1)[non_ignored].float()
@@ -21,3 +22,65 @@ class FocalLoss2d(nn.Module):
         targets = torch.clamp(targets, eps, 1. - eps)
         pt = (1 - targets) * (1 - outputs) + targets * outputs
         return (-(1. - pt)**self.gamma * torch.log(pt)).mean()
+
+class BinaryFocalLoss(nn.Module):
+    """
+    This is a implementation of Focal Loss with smooth label cross entropy supported which is proposed in
+    'Focal Loss for Dense Object Detection. (https://arxiv.org/abs/1708.02002)'
+        Focal_Loss= -1*alpha*(1-pt)*log(pt)
+    :param num_class:
+    :param alpha: (tensor) 3D or 4D the scalar factor for this criterion
+    :param gamma: (float,double) gamma > 0 reduces the relative loss for well-classified examples (p>0.5) putting more
+                    focus on hard misclassified example
+    :param reduction: `none`|`mean`|`sum`
+    :param **kwargs
+        balance_index: (int) balance class index, should be specific when alpha is float
+    """
+
+    def __init__(self, alpha=[1.0, 1.0], gamma=2, ignore_index=None, reduction='mean'):
+        super(BinaryFocalLoss, self).__init__()
+        if alpha is None:
+            alpha = [0.25, 0.75]
+        self.alpha = alpha
+        self.gamma = gamma
+        self.smooth = 1e-6
+        self.ignore_index = ignore_index
+        self.reduction = reduction
+
+        assert self.reduction in ['none', 'mean', 'sum']
+
+        if self.alpha is None:
+            self.alpha = torch.ones(2)
+        elif isinstance(self.alpha, (list, np.ndarray)):
+            self.alpha = np.asarray(self.alpha)
+            self.alpha = np.reshape(self.alpha, (2))
+            assert self.alpha.shape[0] == 2, \
+                'the `alpha` shape is not match the number of class'
+        elif isinstance(self.alpha, (float, int)):
+            self.alpha = np.asarray([self.alpha, 1.0 - self.alpha], dtype=np.float).view(2)
+
+        else:
+            raise TypeError('{} not supported'.format(type(self.alpha)))
+
+    def forward(self, output, target):
+#         prob = torch.sigmoid(output)
+        prob = output
+        prob = torch.clamp(prob, self.smooth, 1.0 - self.smooth)
+
+        pos_mask = (target == 1).float()
+        neg_mask = (target == 0).float()
+
+        pos_loss = -self.alpha[0] * torch.pow(torch.sub(1.0, prob), self.gamma) * torch.log(prob) * pos_mask
+        neg_loss = -self.alpha[1] * torch.pow(prob, self.gamma) * \
+                   torch.log(torch.sub(1.0, prob)) * neg_mask
+
+        neg_loss = neg_loss.sum()
+        pos_loss = pos_loss.sum()
+        num_pos = pos_mask.view(pos_mask.size(0), -1).sum()
+        num_neg = neg_mask.view(neg_mask.size(0), -1).sum()
+
+        if num_pos == 0:
+            loss = neg_loss
+        else:
+            loss = pos_loss / num_pos + neg_loss / num_neg
+        return loss
