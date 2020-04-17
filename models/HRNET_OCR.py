79dfb2bc9a85487f5a5838298297ca12a2f963a2
Keewon Shin <kevinkwshin@gmail.com>
2020-04-17 16:01:46 +0900

    Update HRNET_OCR.py

--- models/HRNET_OCR.py
+++ models/HRNET_OCR.py
@@ -13,8 +13,17 @@ import torch
 import torch.nn as nn
 import torch._utils
 import torch.nn.functional as F
+import torch
+import functools
 
-from .bn_helper import BatchNorm2d, BatchNorm2d_class, relu_inplace
+if torch.__version__.startswith('0'):
+    from .sync_bn.inplace_abn.bn import InPlaceABNSync
+    BatchNorm2d = functools.partial(InPlaceABNSync, activation='none')
+    BatchNorm2d_class = InPlaceABNSync
+    relu_inplace = False
+else:
+    BatchNorm2d_class = BatchNorm2d = torch.nn.SyncBatchNorm
+    relu_inplace = True
 
 ALIGN_CORNERS = True
 BN_MOMENTUM = 0.1
