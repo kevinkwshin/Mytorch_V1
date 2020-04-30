fefddf8cc80dc99ab2ba7d614b5a1303ef502214
Keewon Shin <kevinkwshin@gmail.com>
2020-05-01 00:00:36 +0900

    Update HRNET_SEG.py

--- models/HRNET_SEG.py
+++ models/HRNET_SEG.py
@@ -255,7 +255,7 @@ class HighResolutionNet(nn.Module):
         super(HighResolutionNet, self).__init__()
         
         # stem net
-        self.conv1 = nn.Conv2d(input_c, 64, kernel_size=3, stride=2, padding=1,bias=False)
+        self.conv1 = nn.Conv2d(input_ch, 64, kernel_size=3, stride=2, padding=1,bias=False)
         self.bn1 = BatchNorm2d(64, momentum=BN_MOMENTUM)
         self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1, bias=False)
         self.bn2 = BatchNorm2d(64, momentum=BN_MOMENTUM)
